e564cf10d38e5df699d408e176b03edb2b0b77e6
papilon10 <140213599+papilon10@users.noreply.github.com>
2024-01-26 13:33:33 -0600

    tercer commit

--- src/main/java/www/lab2p2_salvadormacias/articulo.java
+++ src/main/java/www/lab2p2_salvadormacias/articulo.java
@@ -4,10 +4,74 @@
  */
 package www.lab2p2_salvadormacias;
 
+import java.util.Date;
+
 /**
  *
  * @author Apple
  */
 public class articulo {
-    
+
+    String titulo;
+    String autor;
+    String tema;
+    Date publicacion;
+    boolean acceso;
+
+    public articulo() {
+    }
+
+    public articulo(String titulo, String autor, String tema, Date publicacion, boolean acceso) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.tema = tema;
+        this.publicacion = publicacion;
+        this.acceso = acceso;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+    public Date getPublicacion() {
+        return publicacion;
+    }
+
+    public void setPublicacion(Date publicacion) {
+        this.publicacion = publicacion;
+    }
+
+    public boolean isAcceso() {
+        return acceso;
+    }
+
+    public void setAcceso(boolean acceso) {
+        this.acceso = acceso;
+    }
+
+    @Override
+    public String toString() {
+        return "articulo{" + "titulo=" + titulo + ", autor=" + autor + ", tema=" + tema + ", publicacion=" + publicacion + ", acceso=" + acceso + '}';
+    }
+
 }
