39b3e1213a7f738395c09c71be8240f982c1f8c6
papilon10 <140213599+papilon10@users.noreply.github.com>
2024-01-26 13:29:08 -0600

    inicio

---
+++ src/main/java/www/lab2p2_salvadormacias/Lab2p2_salvadormacias.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package www.lab2p2_salvadormacias;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Apple
+ */
+public class Lab2p2_salvadormacias {
+
+    public static Scanner lea = new Scanner(System.in);
+    public static Scanner str = new Scanner(System.in);
+
+    public static void main(String[] args) {
+    }
+}
