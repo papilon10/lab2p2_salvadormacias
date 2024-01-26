1b24ec40e67cd98a4de1bc0a018d7ccb315d4b51
papilon10 <140213599+papilon10@users.noreply.github.com>
2024-01-26 13:39:42 -0600

    sexto commit

--- src/main/java/www/lab2p2_salvadormacias/usuario.java
+++ src/main/java/www/lab2p2_salvadormacias/usuario.java
@@ -9,5 +9,47 @@ package www.lab2p2_salvadormacias;
  * @author Apple
  */
 public class usuario {
-    
+
+    String nombre;
+    String contrasena;
+    String tipo;
+
+    public usuario() {
+    }
+
+    public usuario(String nombre, String contrasena, String tipo) {
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "usuario{" + "nombre=" + nombre + ", contrasena=" + contrasena + ", tipo=" + tipo + '}';
+    }
+
 }
